88f8d1ae24f201fedf78a8ca9828d8e49717e60f
Virginie-Villard <virginie.villard@le-campus-numerique.fr>
2021-09-14 17:02:04 +0200

    Rename index.php en billet.php pour autoload

--- usages.php
+++ usages.php
@@ -1,2 +1,20 @@
 <?php
 
+// On crée une pile d'autoload avec le spl_autoload
+spl_autoload_register('requireClass');
+
+// On crée une fonction pour faire les require de Classe
+function requireClass($classe){
+    require $classe . '.php';
+}
+
+// création de $billet1
+$billet1 = new Billet(["titre", "contenu");
+var_dump($billet1);
+
+$bdd = new PDO('mysql:host=localhost;dbname=Billetsmiguel;charset=utf8', 'Vg','toto', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+$pm = new BilletManager($bdd);
+var_dump($pm->getList());
+
+?>
